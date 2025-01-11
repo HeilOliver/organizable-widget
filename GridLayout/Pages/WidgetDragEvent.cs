d48672c41a83ba2705c4f205514d2c07a67c1f61
OliverHeil <oliver.heil@jentschmann.ch>
2025-01-11 03:49:44 +0100

    Init

---
+++ GridLayout/Pages/WidgetDragEvent.cs
@@ -0,0 +1,25 @@
+﻿namespace GridLayout.Pages;
+
+public class WidgetDragEvent : EventArgs
+{
+
+    public DOMRect Widget { get; set; }
+    
+    public DOMRect Container { get; set; }
+    
+    public double OffsetX { get; set; }
+    
+    public double OffsetY { get; set; }
+}
+
+public class DOMRect
+{
+    public double X { get; set; }
+    
+    
+    public double Y { get; set; }
+    
+    public double Width { get; set; }
+    
+    public double Height { get; set; }
+}
---
+++ GridLayout/wwwroot/js/interop.js
@@ -0,0 +1,41 @@
+﻿
+
+window.setupDragHandler = (widget, container, dotNetRef) => {
+    let initialMouseX, initialMouseY;
+
+    widget.addEventListener('dragstart', (e) => {
+        const widgetRect = widget.getBoundingClientRect();
+
+        initialMouseX = e.clientX - widgetRect.x;
+        initialMouseY = e.clientY - widgetRect.y;
+    });
+
+    widget.addEventListener('drag', (e) => {
+        const widgetRect = widget.getBoundingClientRect();
+        const containerRect = container.getBoundingClientRect();
+        
+        const offsetX = e.clientX - initialMouseX - containerRect.x;
+        const offsetY = e.clientY - initialMouseY - containerRect.y;
+        
+        if (e.clientY ===  0 || e.clientX === 0) {
+            return;
+        }
+        
+        dotNetRef.invokeMethodAsync('HandleDrag', {
+            widget: {
+                x: widgetRect.x,
+                y: widgetRect.y,
+                width: widgetRect.width,
+                height: widgetRect.height
+            },
+            container: {
+                x: containerRect.x,
+                y: containerRect.y,
+                width: containerRect.width,
+                height: containerRect.height
+            },
+            offsetX: offsetX,
+            offsetY: offsetY
+        });
+    });
+};
